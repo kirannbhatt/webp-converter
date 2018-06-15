a8c892fe8dac1f6fdc0c4b4b9f9594676cfe8e95
Kiran Bhatt <kbhatt7638@outlook.com>
2018-06-15 19:59:34 +0545

    file type checker added

---
+++ src/checkFileType.js
@@ -0,0 +1,17 @@
+import path from 'path'
+
+function checkFileType(file,cb){
+    const fileType = /jpg|jpeg/
+
+    const extname = fileType.test(path.extname(file.originalname).toLowerCase())
+
+    const mimetype = fileType.test(file.mimetype)
+
+    if(mimetype && extname) {
+        return cb(null, true)
+    } else{
+        cb('.jpg or .jpeg files only')
+    }
+}
+
+export default checkFileType
