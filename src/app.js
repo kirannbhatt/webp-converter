d8b197b7dd703d408e83b3464b35a98f5541cf2f
Kiran Bhatt <kbhatt7638@outlook.com>
2018-06-15 19:48:00 +0545

    view engine setup in main app

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ import express from 'express'
 
 const app = express();
 
+// view engine setup
+app.set('view engine', 'ejs')
+
+// public folder
+app.use(express.static('./public'))
+
 app.get('/', (req, res) => {
-	res.send('Hello,world!');
+	res.render('index')
 });
 
 export default app
